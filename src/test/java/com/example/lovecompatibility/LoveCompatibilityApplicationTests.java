01efa32245fea82593acd2475721b2812dd546a3
karahan6 <karahanmehmet6@gmail.com>
2022-08-17 19:24:43 +0300

    Initial commit

---
+++ src/test/java/com/example/lovecompatibility/LoveCompatibilityApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.lovecompatibility;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoveCompatibilityApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
